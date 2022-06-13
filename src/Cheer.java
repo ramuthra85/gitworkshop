35678e4610d7eed9f5d657952ca411f376846a0d
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2022-06-13 11:18:16 +0200

    c1

---
+++ src/Cheer.java
@@ -0,0 +1,9 @@
+public class Cheer {
+    public static void main(String[] args) {
+        cheer();
+    }
+
+    public static void cheer() {
+        System.out.println("Apocalyptica");
+    }
+}
