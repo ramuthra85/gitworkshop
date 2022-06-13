1da457c01e427d7f99bdb0c3700b28b747e96a3d
Tamas Solymosi <tamssolymosi@yahoo.com>
2022-06-13 13:46:47 +0200

    b5

--- src/Count.java
+++ src/Count.java
@@ -5,14 +5,9 @@ public class Count {
 
     }
 
-<<<<<<< HEAD
         public static void count(int monthOfBirth, String name) {
-            System.out.println("The month in which I was born *: " + monthOfBirth);
-            System.out.println("My name is " + name);
-=======
-        public static void count(int monthOfBirth) {
             System.out.println("Concluded text - The month in which I was born: " + monthOfBirth);
->>>>>>> 8f03e0d0f6375ac1292158a5e5cdcb09ebece3a6
+            System.out.println("My name is " + name);
         }
     }
 
