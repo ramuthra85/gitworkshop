880ba18689055fdf600b18a059c9e82771220ac2
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2022-06-13 14:04:16 +0200

    faszom

--- src/Count.java
+++ src/Count.java
@@ -6,7 +6,7 @@ public class Count {
     }
 
         public static void count(int monthOfBirth, String name) {
-            System.out.println("Concluded text - The month in which I was born: " + monthOfBirth);
+            System.out.println("faszomtudjamitkellideirni: " + monthOfBirth);
             System.out.println("My name is " + name);
         }
     }
