08a9f557f40ba6be8c645ad2f695f74f58a0bdec
varta5 <varga.tamas.pro@gmail.com>
2022-06-13 12:07:32 +0200

    a3

--- src/Count.java
+++ src/Count.java
@@ -1,12 +1,12 @@
 public class Count {
     public static void main(String[] args) {
 
-        count(37);
+        count(12);
 
     }
 
-        public static void count(int age) {
-            System.out.println("My age is " + age);
+        public static void count(int monthOfBirth) {
+            System.out.println("The month in which I was born: " + monthOfBirth);
         }
     }
 
